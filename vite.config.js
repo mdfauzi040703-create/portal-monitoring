383f69d5d2005195d5b2d9046605f880b1358c98
Fauzi <fauzi531@gmail.com>
2026-06-10 20:14:51 +0700

    update routes and vite config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
 import { defineConfig } from 'vite'
 import laravel from 'laravel-vite-plugin'
+import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  plugins: [laravel(['resources/js/app.js'])],
-  server: {
-  host: true,
-  allowedHosts: ['renegade-italicize-fraction.ngrok-free.dev']
-}
-
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+        vue(),
+    ],
+    resolve: {
+        alias: {
+            vue: 'vue/dist/vue.esm-bundler.js',
+        },
+    },
 })
