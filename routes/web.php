420f02ab9182084d955ae6569630d1ed55681795
Fauzi <fauzi531@gmail.com>
2026-06-13 22:19:49 +0700

    fix route order

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,6 @@
 <?php
 use Illuminate\Support\Facades\Route;
 
-Route::get('/{any}', function () {
-    return view('welcome');
-})->where('any', '.*');
-
 Route::get('/seed-users', function () {
     if (\App\Models\User::where('email', 'md.fauzi040703@gmail.com')->exists()) {
         return 'User sudah ada!';
@@ -17,3 +13,7 @@ Route::get('/seed-users', function () {
 
     return 'Berhasil membuat 4 user! Asisten Manager: md.fauzi040703@gmail.com / bacot123';
 });
+
+Route::get('/{any}', function () {
+    return view('welcome');
+})->where('any', '.*');
