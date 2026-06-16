f02d63886379e40c89d0af174775433eb8c0da6d
Fauzi <fauzi531@gmail.com>
2026-06-16 12:22:37 +0700

    add manual notify trigger

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,11 @@
 <?php
 use Illuminate\Support\Facades\Route;
+
+Route::get('/run-notify', function () {
+    \Illuminate\Support\Facades\Artisan::call('notify:warnings');
+    return 'Notifikasi sudah dijalankan! Cek log notifikasi dan email/WA.';
+});
+
 Route::get('/{any}', function () {
     return view('welcome');
 })->where('any', '.*');
