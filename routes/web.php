af999602e57aa3a454d4689fcf893e690670a017
Fauzi <fauzi531@gmail.com>
2026-06-16 14:00:03 +0700

    clean up test routes, keep secure cron endpoint

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,9 @@
 <?php
 use Illuminate\Support\Facades\Route;
 
-Route::get('/clear-config', function () {
-    \Illuminate\Support\Facades\Artisan::call('config:clear');
-    return 'Config cache cleared! CRON_SECRET sekarang: ' . env('CRON_SECRET', 'TIDAK ADA');
-});
-
 Route::get('/cron/notify-warnings/{secret}', function ($secret) {
-    $expected = env('CRON_SECRET', 'ganti-rahasia-ini');
-    if ($secret !== $expected) {
-        return "Secret tidak cocok. Yang dikirim: {$secret} | Yang diharapkan: {$expected}";
+    if ($secret !== env('CRON_SECRET', 'ganti-rahasia-ini')) {
+        abort(403, 'Unauthorized');
     }
     \Illuminate\Support\Facades\Artisan::call('notify:warnings');
     $output = \Illuminate\Support\Facades\Artisan::output();
