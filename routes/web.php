7870ce8db539e5b98c3b5747121f9d9d03a8d5b1
Fauzi <fauzi531@gmail.com>
2026-06-17 19:46:22 +0700

    clean up test and debug routes

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,6 @@
 <?php
 use Illuminate\Support\Facades\Route;
 
-Route::get('/check-migrations', function () {
-    $migrations = \Illuminate\Support\Facades\DB::table('migrations')
-        ->orderBy('id', 'desc')
-        ->limit(10)
-        ->pluck('migration');
-    return '<pre>' . implode("\n", $migrations->toArray()) . '</pre>';
-});
-
 Route::get('/cron/notify-warnings/{secret}', function ($secret) {
     if ($secret !== env('CRON_SECRET', 'ganti-rahasia-ini')) {
         abort(403, 'Unauthorized');
@@ -17,16 +9,6 @@ Route::get('/cron/notify-warnings/{secret}', function ($secret) {
     $output = \Illuminate\Support\Facades\Artisan::output();
     $sentCount = substr_count($output, 'terkirim ke');
     return "OK. {$sentCount} notifikasi diproses pada " . now()->format('d M Y H:i');
-});
-
-Route::get('/force-migrate', function () {
-    try {
-        \Illuminate\Support\Facades\Artisan::call('migrate', ['--force' => true]);
-        $output = \Illuminate\Support\Facades\Artisan::output();
-        return '<pre>' . $output . '</pre>';
-    } catch (\Exception $e) {
-        return 'ERROR: ' . $e->getMessage();
-    }
 });
 
 Route::get('/{any}', function () {
