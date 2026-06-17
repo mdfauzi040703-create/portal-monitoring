1eb34e59a2c94c0956d874cd5205c855f5e67bdb
Fauzi <fauzi531@gmail.com>
2026-06-17 19:40:09 +0700

    remove duplicate migration, add check-migrations route

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,6 @@
 <?php
 use Illuminate\Support\Facades\Route;
 
-Route::get('/cron/notify-warnings/{secret}', function ($secret) {
-    if ($secret !== env('CRON_SECRET', 'ganti-rahasia-ini')) {
-        abort(403, 'Unauthorized');
-    }
-    \Illuminate\Support\Facades\Artisan::call('notify:warnings');
-    $output = \Illuminate\Support\Facades\Artisan::output();
-
-    // Hitung ringkasan saja, jangan kirim full log ke response
-    $sentCount = substr_count($output, 'terkirim ke');
-
-    return "OK. {$sentCount} notifikasi diproses pada " . now()->format('d M Y H:i');
-});
-
-Route::get('/{any}', function () {
-    return view('welcome');
-})->where('any', '.*');
-
 Route::get('/check-migrations', function () {
     $migrations = \Illuminate\Support\Facades\DB::table('migrations')
         ->orderBy('id', 'desc')
@@ -25,3 +8,17 @@ Route::get('/check-migrations', function () {
         ->pluck('migration');
     return '<pre>' . implode("\n", $migrations->toArray()) . '</pre>';
 });
+
+Route::get('/cron/notify-warnings/{secret}', function ($secret) {
+    if ($secret !== env('CRON_SECRET', 'ganti-rahasia-ini')) {
+        abort(403, 'Unauthorized');
+    }
+    \Illuminate\Support\Facades\Artisan::call('notify:warnings');
+    $output = \Illuminate\Support\Facades\Artisan::output();
+    $sentCount = substr_count($output, 'terkirim ke');
+    return "OK. {$sentCount} notifikasi diproses pada " . now()->format('d M Y H:i');
+});
+
+Route::get('/{any}', function () {
+    return view('welcome');
+})->where('any', '.*');
