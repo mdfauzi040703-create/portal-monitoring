bdc0fd4228a5bb0cc8f186ba7c04a89cbc927576
Fauzi <fauzi531@gmail.com>
2026-06-19 19:58:06 +0700

    perubahan dashboard manager

--- resources/js/views/manager/LogNotifikasi.vue
+++ resources/js/views/manager/LogNotifikasi.vue
@@ -19,7 +19,7 @@
             <div class="text-xs text-gray-500 mt-0.5">
               PIC: <span class="font-medium text-blue-600">{{ doc.pic?.name }}</span>
             </div>
-            <a v-if="doc.file_path" :href="'http://localhost:8000/storage/' + doc.file_path" target="_blank"
+            <a v-if="doc.file_path" :href="'/storage/' + doc.file_path" target="_blank"
               class="inline-flex items-center gap-1 text-xs text-blue-600 hover:underline mt-1">
               <svg class="w-3 h-3" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 10v6m0 0l-3-3m3 3l3-3m2 8H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"/></svg>
               Lihat file
