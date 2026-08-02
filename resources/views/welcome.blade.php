e2c525ca876b4cfcfa086cc22625aab43cdc8433
Fauzi <fauzi531@gmail.com>
2026-08-02 17:14:20 +0700

    feat: tambah favicon logo PLN

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Portal Monitoring</title>
+    <link rel="icon" type="image/x-icon" href="/favicon.ico">
     <script src="https://cdn.tailwindcss.com"></script>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
